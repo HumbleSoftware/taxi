6c53149cdd07092653820ac974a4b55d1223f862
Carl Sutherland <carl@humblesoftware.com>
2012-11-23 13:09:10 -0500

    Added vanilla and bdd examples.

---
+++ examples/bdd.js
@@ -0,0 +1,21 @@
+driver('Component One', function () {
+  passenger('One Default', function (options) {
+    var
+      $container = options.$container;
+    $container.html('hello world');
+  });
+  passenger('One Special', function (options) {
+    var
+      $container = options.$container;
+    $container.html('hello world');
+  });
+});
+driver('Component Two', function () {
+  beforeEach(function (options) {
+    options.$container.append('<p>some common html</p>');
+  });
+  passenger('Default Content', function () {});
+  passenger('Special Content', function (options) {
+    options.$container.append('<p>extra special content</p>');
+  });
+});
