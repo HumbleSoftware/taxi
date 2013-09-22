9359c5d3fbefabd0e0e4630eb0f777f00537388c
Gabriel Raineri <graineri@gmail.com>
2013-09-22 15:42:27 -0300

    Adding Driver Collection Tests

--- test/bdd.js
+++ test/bdd.js
@@ -303,3 +303,13 @@ describe('Driver Model', function () {
     });
   });
 });
+
+describe('Driver Collection', function () {
+  it('should contain Driver Models', function () {
+    var
+      driverCollection = new taxi.DriverCollection({ 
+        key: '1'
+      });
+    expect(driverCollection.get('1')).to.be.an.instanceOf(taxi.DriverModel);
+  });
+});
